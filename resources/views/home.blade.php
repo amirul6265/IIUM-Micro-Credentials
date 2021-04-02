55897994262f681465252f7c08ff1ab3a71510e3
amirul6265 <45592688+amirul6265@users.noreply.github.com>
2021-04-02 11:30:10 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,13 +86,13 @@
         <hr class="featurette-divider">
         <!-- Three columns of text below the carousel -->
         <div class="row-hots" id="course_categories">
-          @foreach($hots as $key => $data)
+          <!-- @foreach($hots as $key => $data)
           <div class="col-lg-4">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
             <h2>{{$data->fullname}}</h2>
             <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
           </div>
-          @endforeach
+          @endforeach -->
         </div>
         <p class="text-right"><a class="btn btn-primary" href="{{ url('/all') }}" role="button">View All &raquo;</a></p>
         <hr class="featurette-divider">
