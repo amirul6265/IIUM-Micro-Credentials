7e8d5044cedbb4a53dd7cb314998629af63088af
amirul6265 <amirulabdullah979797@gmail.com>
2021-04-02 14:49:33 +0800

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,11 +86,7 @@
         <hr class="featurette-divider">
         <!-- Three columns of text below the carousel -->
         <div class="row" id="course_categories">
-<<<<<<< HEAD
           @foreach($hots as $key => $data)
-=======
-           @foreach($hots as $key => $data)
->>>>>>> cdf12bdf1c88d89659bef8d7da359d655d7fb1ee
           <div class="col-lg-4">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
             <h2>{{$data->fullname}}</h2>
